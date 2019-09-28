df253cc0e6524fc0f4573a693ac919fbbab053fc
Dustray <yinxiyue1995@outlook.com>
2019-09-28 19:31:57 +0800

    设置操作逻辑

--- BitLockerUI/SetPasswordWindow.xaml.cs
+++ BitLockerUI/SetPasswordWindow.xaml.cs
@@ -19,25 +19,77 @@ namespace BitLockerUI
     /// </summary>
     public partial class SetPasswordWindow : BaseWindow
     {
-
+        private int _maxSize  = 20;
+        private int _minSize = 4;
+        private bool IsSimplePass = false; 
         public SetPasswordWindow()
         {
             InitializeComponent();
+            pboxPassword1.MaxLength = 32;
+            pboxPassword2.MaxLength = 32;
         }
 
+        #region 成员控制
         private void cboxIsSimplePassword_Checked(object sender, RoutedEventArgs e)
         {
-            pboxPassword1.Clear();
-            pboxPassword2.Clear();
+            _maxSize = 4;
+            IsSimplePass = true;
+            ChangeToSimplePassword(true);
         }
         private void cboxIsSimplePassword_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            IsSimplePass = false;
+            ChangeToSimplePassword(false);
         }
 
         private void PboxPassword_PasswordChanged(object sender, RoutedEventArgs e)
         {
+            bool flag = pboxPassword1.Password.Length >= 4 && pboxPassword2.Password.Length >= 4;
+            if (IsSimplePass)
+            {
+                flag = flag && (pboxPassword1.Password.Length <= 4) && (pboxPassword2.Password.Length <= 4);
+            }
+            else
+            {
+                flag = flag && (pboxPassword1.Password.Length <= 32) && (pboxPassword2.Password.Length <= 32);
+            }
+            flag = flag && (pboxPassword1.Password == pboxPassword2.Password);
+            btnSubmit.IsEnabled = flag;
+        }
+
+        private void ChangeToSimplePassword(bool flag)
+        {
+
+            pboxPassword1.Clear();
+            pboxPassword2.Clear();
+            pboxPassword1.MaxLength = pboxPassword2.MaxLength = flag ? 4 : 32;
+            pboxPassword2.MaxLength = pboxPassword2.MaxLength = flag ? 4 : 32;
+
 
         }
+
+        private void pboxPassword1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = CanBeInput(e.Text);
+        }
+
+        private void pboxPassword2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = CanBeInput(e.Text);
+        }
+
+        private bool CanBeInput(string str)
+        {
+            if (IsSimplePass)
+            {
+                if (!int.TryParse(str, out int a))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
     }
 }
