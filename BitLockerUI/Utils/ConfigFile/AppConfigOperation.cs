896ba127df0db7f291694eb57a90f68a9d924339
Dustray <yinxiyue1995@outlook.com>
2019-09-28 10:15:14 +0800

    config操作；忽略文件

---
+++ BitLockerUI/Utils/ConfigFile/AppConfigOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitLockerUI.Utils.ConfigFile
+{
+    class AppConfigOperation
+    {
+        public string Read(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+        public void write(string key,string value)
+        {
+            Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            cfa.AppSettings.Settings[key].Value = value;
+            cfa.Save();
+        }
+    }
+}
--- BitLockerUI/Utils/ConfigFile/XMLOperation.cs
+++ BitLockerUI/Utils/ConfigFile/XMLOperation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,9 @@ namespace BitLockerUI.Utils.ConfigFile
         //        <unlockpassword>fdsahgfjghfdhgfdhgfdsh==</unlockpassword>
         //    </user>    
         //</root>
+        /// <summary>
+        /// 创建新的文件
+        /// </summary>
         public void CreateNewFile()
         {
             var rootNode = _xml.CreateElement("root");
@@ -39,6 +43,34 @@ namespace BitLockerUI.Utils.ConfigFile
             userNode.AppendChild(scretpasswordNode);
             userNode.AppendChild(unlockpasswordNode);
             _xml.Save(path);
+        }
+        /// <summary>
+        /// 获取指定节点数据
+        /// </summary>
+        /// <param name="nodeLevel"></param>
+        /// <returns></returns>
+        public string GetNodeValue(string nodeLevel)
+        {
+            //string result = "";
+            XmlNode root = _xml.SelectSingleNode(nodeLevel);//   /root/user
+            //XmlNodeList childlist = root.ChildNodes;
+            return root.InnerText;
+        }
+
+        /// <summary>
+        /// 设置指定节点数据
+        /// </summary>
+        /// <param name="nodeLevel"></param>
+        /// <param name="value"></param>
+        public void SetNodeValue(string nodeLevel,string value)
+        {
+            if (!File.Exists(path))
+            {
+                CreateNewFile();
+            }
+            XmlNode root = _xml.SelectSingleNode(nodeLevel);//   /root/user
+            //XmlNodeList childlist = root.ChildNodes;
+            root.InnerText=value;
         }
 
     }
